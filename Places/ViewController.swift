a086fa7fe159bd9ac3a427ab505b05837efe86f9
yezzziam <yezzziam@gmail.com>
2020-01-21 16:04:32 +0200

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Yegor Zubarets on 1/21/20.
+//  Copyright © 2020 Yegor Zubarets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
