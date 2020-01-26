b7ac0f2d52b27ef5731c710e03886600469a656e
yezzziam <yezzziam@gmail.com>
2020-01-26 14:38:03 +0200

    Cusom Table View Cell

---
+++ Places/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  Places
+//
+//  Created by Yegor Zubarets on 1/26/20.
+//  Copyright © 2020 Yegor Zubarets. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var locationLabel: UILabel!
+    
+    @IBOutlet var typeLabel: UILabel!
+}
